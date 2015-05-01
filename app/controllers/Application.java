cdec2fc1d5737674ccd48afef77fc63311e0dfc6
jsc <jsc@scireum.de>
2015-05-01 21:04:31 +0200

    Added bootstrap and list activities

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,15 +2,35 @@ package controllers;
 
 
 import helper.HarmonyInstance;
+import net.whistlingfish.harmony.config.Activity;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Created by jsc on 26.04.15.
  */
 public class Application extends Controller {
     public static Result index() {
-        return ok(views.html.index.render(HarmonyInstance.getClient().getConfig().getActivities()));
+        List<Activity> listActivities = HarmonyInstance.getClient().getConfig().getActivities();
+        Collections.sort(listActivities, new Comparator<Activity>() {
+            @Override
+            public int compare(Activity o1, Activity o2) {
+                Integer order1 = o1.getActivityOrder(), order2 = o2.getActivityOrder();
+                if (order1 == null ^ order2 == null) {
+                    return (order1 == null) ? -1 : 1;
+                }
+
+                if (order1 == null && order2 == null) {
+                    return 0;
+                }
+                return order1.compareTo(order2);
+            }
+        });
+        return ok(views.html.index.render(HarmonyInstance.getClient().getCurrentActivity(), listActivities));
     }
 
 }
